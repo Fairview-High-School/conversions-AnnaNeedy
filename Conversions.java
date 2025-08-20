c1340beb808fb0e5b2ddd553dd6add50e63cb690
AnnaNeedy <alneedy01@bvsd.org>
2025-08-20 18:57:38 +0000

    Added Menu Items

--- Conversions.java
+++ Conversions.java
@@ -1,1 +1,16 @@
+// Anna Needy
+// 08/20/25
 
+public class Conversions
+{
+    public static void main(String[] args)
+    {
+        System.out.println("1. Fahrenheit to Celcius");
+        System.out.println("2. Celcius to Fahrenheit");
+        System.out.println("3. Feet to Meters");
+        System.out.println("4. Meters to Feet");
+        System.out.println("5. Ounces to Milliliters");
+        System.out.println("6. Milliliters to Ounces");
+
+    }
+}
